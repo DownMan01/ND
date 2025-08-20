1b7d1c0a0d885306ca2651f513920a874a585b83
J <98541904+DownMan01@users.noreply.github.com>
2025-08-20 18:19:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     icon: "/favicon.png",
     apple: "/favicon.png",
   },
-    generator: 'v0.dev'
+    generator: ''
 }
 
 export default function RootLayout({
